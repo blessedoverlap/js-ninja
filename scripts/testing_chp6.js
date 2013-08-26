161fbd59128dec256ffe4da49c1ef65c29090d0f
Terry Tong <blessedoverlap@gmail.com>
2013-08-26 17:15:47 +0800

    Completed Chapter 6

--- scripts/testing_chp6.js
+++ scripts/testing_chp6.js
@@ -47,18 +47,96 @@
  assert(ninja !== ninja2, "But not the same Ninja!");
  */
 
-function Person() {
+/*
+ function Person() {
+ }
+
+ Person.prototype.dance = function() {
+ };
+ function Ninja() {
+ }
+
+ Ninja.prototype = {
+ dance : Person.prototype.dance
+ };
+ var ninja = new Ninja();
+ assert( ninja instanceof Ninja, "ninja receives functionality from the Ninja prototype");
+ assert( ninja instanceof Person, "... and the Person prototype");
+ assert( ninja instanceof Object, "... and the Object prototype");
+ */
+
+/*
+ function Person() {
+ }
+
+ Person.prototype.dance = function() {
+ };
+
+ function Ninja() {
+ }
+
+ Ninja.prototype = new Person();
+ var ninja = new Ninja();
+ assert( ninja instanceof Ninja, "ninja receives functionality from the Ninja prototype");
+ assert( ninja instanceof Person, "... and the Person prototype");
+ assert( ninja instanceof Object, "... and the Object prototype");
+ assert( typeof ninja.dance == "function", "... and can dance!")
+ */
+
+/*
+ if (!Array.prototype.forEach) {
+ Array.prototype.forEach = function(callback, context) {
+ for (var i = 0; i < this.length; i++) {
+ callback.call(context || null, this[i], i, this);
+ }
+ };
+ } else {
+ assert(true, "This browser supports Array#forEach");
+ }
+
+ ["a", "b", "c"].forEach(function(value, index, array) {
+ assert(value, "Is in position " + index + " out of " + (array.length - 1));
+ });
+ */
+
+/*
+ Object.prototype.keys = function() {
+ var keys = [];
+ for (var p in this) {
+ if (this.hasOwnProperty(p)) keys.push(p);
+ }
+ console.log(keys.length);
+ return keys;
+ }
+
+ var obj = { a: 1, b: 2, c: 3 };
+ assert(obj.keys().length == 3, "obj has three keys");
+ */
+
+/*
+ Number.prototype.add = function(num){
+ return this + num;
+ };
+
+ var n = 5;
+ assert(n.add(3) == 8, "It works when the number is in a variable.");
+ assert((5).add(3) == 8, "Also works if a number is wrapped in parentheses.");
+ assert(5.add(3) == 8, "What about a simple literal?");
+ */
+
+/*
+ function User(first, last) {
+ this.name = first + " " + last;
+ }
+
+ var name = "Rukia";
+ var user = User("Ichigo", "Kurosaki");
+ assert(name == "Rukia", "Name was set to Rukia.");
+ */
+
+function Test() {
+  return this instanceof arguments.callee;
 }
 
-Person.prototype.dance = function() {
-};
-function Ninja() {
-}
-
-Ninja.prototype = {
-  dance : Person.prototype.dance
-};
-var ninja = new Ninja();
-assert( ninja instanceof Ninja, "ninja receives functionality from the Ninja prototype");
-assert( ninja instanceof Person, "... and the Person prototype");
-assert( ninja instanceof Object, "... and the Object prototype");
+assert(!Test(), "We didn't instantiate, so it returns false.");
+assert(new Test(), "We did instantiate, returning true.");
